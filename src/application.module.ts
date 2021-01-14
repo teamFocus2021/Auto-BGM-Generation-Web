efc898ca307f46e31290a3c482797f996bdb64bc
Deokk <deokk1112@gmail.com>
2021-01-14 23:40:01 +0900

    First Commit (Boilerplate)

---
+++ src/application.module.ts
@@ -0,0 +1,18 @@
+import { Module } from '@nestjs/common';
+import { RenderModule } from 'nest-next';
+import Next from 'next';
+import { AppController } from './app.controller';
+
+@Module({
+  imports: [
+    RenderModule.forRootAsync(
+      Next({
+        dev: process.env.NODE_ENV !== 'production',
+        conf: { useFilesystemPublicRoutes: false },
+      }),
+    ),
+  ],
+  controllers: [AppController],
+  providers: [],
+})
+export class AppModule {}
