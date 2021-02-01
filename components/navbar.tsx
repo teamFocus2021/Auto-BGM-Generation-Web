d9bb46afd2b9bef73c1713ad00267de70a61fe18
ire4564 <ire4564@gmail.com>
2021-02-01 10:32:36 +0900

    Add png files and modify background

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -3,12 +3,12 @@ import Link from 'next/link';
 
 const Navbar: FC = () => {
   return (
-    <div style={{ display: 'flex', marginTop: "40px", marginBottom: "40px", marginLeft: "20px", marginRight: "70px" }}>
+    <div style={{ display: 'flex', marginTop: "40px", marginBottom: "40px", marginLeft: "20px", marginRight: "30px" }}>
       <div style={{ flexBasis: '80%' }}>
         <Link href="/views/home" as="/">
           <a>
             {/*태그가 안 먹힘, 노래 목록도 마찬가지, 도메인 접속에 문제가 있는지 확인할 것*/}
-            <img className="logo" src='http://sehwa98.dothome.co.kr/mp3/logo.png' />
+            <img style={{ width: "270px", height: "75px"}} className="logo" src='http://sehwa98.dothome.co.kr/mp3/logo.png' />
           </a>
         </Link>
       </div>
