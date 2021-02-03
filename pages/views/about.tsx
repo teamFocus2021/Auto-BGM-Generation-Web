bc8d7528880b76eb34b314cd2ce155702b331833
ire4564 <ire4564@gmail.com>
2021-02-03 15:32:17 +0900

    Modify Detail page css

--- pages/views/about.tsx
+++ pages/views/about.tsx
@@ -2,9 +2,9 @@ import * as React from 'react';
 import { NextPage } from 'next';
 
 const Page: NextPage = () => {
-  return <div className="main_div">
-  <h2>PROJECT DETAIL</h2>
-  <img style={{ width: "700px", height: "500px", marginTop:"-5rem" }} src='http://sarac33.dothome.co.kr/icon.png' />
+  return <div className="align">
+  <h2 style={{textAlign: "center"}}>PROJECT DETAIL</h2>
+  <img style={{display: "block", margin: "0px auto", marginTop: "-2rem", width: "1000px", height: "450px"}} src='http://sarac33.dothome.co.kr/icon.png' />
   <p className="explanation">
     Our program tracks your face in your video and suggests music to match the mood of your video!<br></br>
     Use this program to save time choosing BGM when editing, and make editing easier.<br></br>
