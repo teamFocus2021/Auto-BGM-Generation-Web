29e487e9e2bc049c686245ef7c60a27a2bb86a2a
ire4564 <ire4564@gmail.com>
2021-02-03 16:50:21 +0900

    Compelete make all front page

--- pages/views/about.tsx
+++ pages/views/about.tsx
@@ -3,7 +3,7 @@ import { NextPage } from 'next';
 
 const Page: NextPage = () => {
   return <div className="align">
-  <h2 style={{textAlign: "center"}}>PROJECT DETAIL</h2>
+  <h2 style={{textAlign: "center", fontFamily: "ArchivoBlack-Regular", fontSize: "40px", color: "#232437"}}>PROJECT DETAIL</h2>
   <img style={{display: "block", margin: "0px auto", marginTop: "-2rem", width: "1000px", height: "450px"}} src='http://sarac33.dothome.co.kr/icon.png' />
   <p className="explanation">
     Our program tracks your face in your video and suggests music to match the mood of your video!<br></br>
--- pages/views/connect.tsx
+++ pages/views/connect.tsx
@@ -2,10 +2,14 @@ import * as React from 'react';
 import { NextPage } from 'next';
 
 const Page: NextPage = () => {
-  return <div className="main_div">
-    <p className="explanation">
-      This is connect page!
+  return <div style={{marginTop: "-8rem"}}>
+    <img style={{display: "block", margin: "0px auto", marginTop: "-2rem", width: "450px", height: "450px"}} src='http://sarac33.dothome.co.kr/bean.png' />
+    <p className="about_text1">
+      If you want to connect with us, please contact us!
     </p>
+    <p className="about_text2">
+      ire4564@gmail.com
+      </p>
   </div>;
 };
 
